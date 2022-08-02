d32c3d172044d119638f04aee1daf68969be3670
Alejandro Jaramillo <60363190+Alexx7050@users.noreply.github.com>
2022-08-02 10:37:54 -0600

    Create switches.h

---
+++ code/switches.h
@@ -0,0 +1,14 @@
+#ifndef switches_included
+#define switches_included
+
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+#define SWITCHES (SW1|SW2|SW3|SW4)
+
+int switches;
+
+void switch_init(); // setup switch
+void switch_interrupt_handler();
+char switch_update_interrupt_sense();
